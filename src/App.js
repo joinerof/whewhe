19e3eb11b0b5a501ef3e9713691ac9dc0ff77eec
Kevin Zheng <zhengke@sheridancollege.ca>
2021-11-24 18:06:14 -0500

    NumberPicker basic functionality done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends React.Component {
       isValidBet: false,
     };
     this.handleBetClick = this.handleBetClick.bind(this);
+    this.handleNumberClick = this.handleNumberClick.bind(this);
+    this.handleGenerateClick = this.handleGenerateClick.bind(this);
   }
 
   static isValidBet(bet, numbers) {
@@ -20,19 +22,41 @@ class App extends React.Component {
   }
 
   handleBetClick(bet) {
-    if (bet === 0) {
-      this.setState({ bet: 0 });
-      return;
-    }
     // still seems to work even if I loop 10k times,
     // this.setState({ bet: this.state.bet + bet });
     this.setState((state) => {
-      const newBet = state.bet + bet;
+      const newBet = bet ? state.bet + bet : 0;
       return {
         bet: newBet,
         isValidBet: App.isValidBet(newBet, state.numbers),
       };
     });
+  }
+
+  handleNumberClick(number) {
+    this.setState((state) => {
+      let newNumbers;
+      if (state.numbers.includes(number)) {
+        newNumbers = state.numbers.filter((e) => e !== number);
+      } else if (state.numbers.length === 3) {
+        newNumbers = state.numbers.slice();
+      } else {
+        newNumbers = state.numbers.concat([number]);
+        newNumbers.sort((a, b) => a - b);
+      }
+      return {
+        numbers: newNumbers,
+        isValidBet: App.isValidBet(state.bet, newNumbers),
+      };
+    });
+  }
+
+  handleGenerateClick() {
+    alert(
+      `RECEIPT:
+Numbers selected: ${this.state.numbers}
+Total paid: ${this.state.bet}`
+    );
   }
 
   render() {
@@ -43,11 +67,18 @@ class App extends React.Component {
         </header>
         <div id="ticketArea">
           <BetAmount bet={this.state.bet} onClick={this.handleBetClick} />
-          <NumberPicker numbers={this.state.numbers} />
+          <NumberPicker
+            numbers={this.state.numbers}
+            onClick={this.handleNumberClick}
+          />
         </div>
         <div id="generateArea">
           <h2>Cash Out</h2>
-          <button id="generate" disabled={!this.state.isValidBet}>
+          <button
+            id="generate"
+            disabled={!this.state.isValidBet}
+            onClick={this.handleGenerateClick}
+          >
             Generate Ticket
           </button>
         </div>
--- src/components/NumberPicker.js
+++ src/components/NumberPicker.js
@@ -4,15 +4,28 @@ import "./NumberPicker.css";
 class NumberPicker extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(e) {
+    this.props.onClick(parseInt(e.target.value));
+  }
+
   render() {
+    const buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((e) => (
+      <button
+        key={e}
+        value={e}
+        onClick={this.handleClick}
+        className={this.props.numbers.includes(e) ? "picked" : null}
+      >
+        {e}
+      </button>
+    ));
     return (
       <section id="numberPicker">
         <h2>Choose 3 numbers</h2>
-        <div id="numbers">
-          <button>1</button>
-          <button>2</button>
-        </div>
+        <div id="numbers">{buttons}</div>
         <div>
           <button>Random</button>
           <button>Clear</button>
